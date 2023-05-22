a7422fc85710f648e3ef31c908cd68166f808aa9
Ananta khanal <anantaramankhanal@gmail.com>
2023-05-22 23:26:45 +0545

    sign up

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,19 +1,28 @@
 import { Component,OnInit } from '@angular/core';
-
+import { SellerService } from '../services/seller.service';
+import { Router} from '@angular/router';
+import { SignUp } from '../data-type';
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css']
 })
 export class SellerAuthComponent implements OnInit {
-  constructor(){}
-  
-  ngOnInit(): void {
-    
-  }
-  signUp(data:object):void
+  constructor(private seller :SellerService,private router:Router)
   {
-    console.warn(data);
+
+  }
+  
+  ngOnInit(): void {}
+
+  signUp(data:SignUp):void
+  {
     alert(data);
+    this.seller.userSignup(data).subscribe((result)=>{
+      console.warn(result);
+      if(result){
+        this.router.navigate(['seller-home'])
+      }
+    });
   }
 }
